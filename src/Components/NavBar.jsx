d916463c41e4ad0cfe720fcd41d7f585776c6bd8
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-04 01:09:18 +0400

    Future/completed NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Logo from "/Logo.svg";
+import notification from "/notif.svg";
+import search from "/search.svg";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <div className="flex justify-center mx-auto max-w-[1440px] w-full h-24 py-5">
+        <div className="max-w-7xl w-full flex justify-between items-center px-20">
+          <div className="">
+            <img src={Logo} alt="Logo" width={165} height={50} />
+          </div>
+          <div className="flex items-center justify-center max-w-[412px] w-full h-[61px] border-[3px] border-[#1F1F1F] rounded-lg bg-[#0F0F0F]">
+            <nav className="">
+              <ul className="flex items-center h-11 gap-4 text-[#BFBFBF]">
+                <li className="">Home</li>
+                <li>Movies & Shows</li>
+                <li>Support</li>
+                <li>Subscriptions</li>
+              </ul>
+            </nav>
+          </div>
+          <div className="flex justify-between max-w-14 w-full h-6">
+            <img src={search} alt="Search" />
+            <img src={notification} alt="notification" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
