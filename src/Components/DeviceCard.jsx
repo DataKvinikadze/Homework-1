40b2978536842870501e3326fc3fae187c78cd7f
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-06 23:59:46 +0400

    feature/complete DeviceCard.svg

--- src/Components/DeviceCard.jsx
+++ src/Components/DeviceCard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const DeviceCard = () => {
-  return <div>DeviceCard</div>;
+const DeviceCard = ({ title, image, description }) => {
+  return (
+    <>
+      <div className="max-w-[413px] w-full bg-[#0F0F0F] rounded-[10px] p-[40px]">
+        <div className="flex gap-3 mb-[24px] bg-[#111111]">
+          <div className="bg-[#111111]">
+            <img src={image} alt="Device Icon" description />
+          </div>
+          <h3>{title}</h3>
+        </div>
+        <p className="text-[#999999]">{description}</p>
+      </div>
+    </>
+  );
 };
 
 export default DeviceCard;
