43c88470ada0ff6e7b58efb66a4d0051afee2026
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-06 22:59:44 +0400

    feature/create Category Card

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <>
+      <div>
+        <div>
+          <div className="text-[#ffffff]">
+            <h2>Explore our wide variety of categories</h2>
+            <p>
+              Whether you're looking for a comedy to make you laugh, a drama to
+              make you think, or a documentary to learn something new
+            </p>
+          </div>
+          <div>
+            <div>Right</div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>Left</div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Category;
