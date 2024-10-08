811c72fdc2c9d06a01c5179ef95095f37f5abdb3
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-08 20:40:45 +0400

    feature/Question Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar";
 import Header from "./Components/Header";
 import Category from "./Components/Category";
 import Devices from "./Components/Devices";
+import Question from "./Components/Question";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Category />
         <Devices />
+        <Question />
       </div>
     </>
   );
