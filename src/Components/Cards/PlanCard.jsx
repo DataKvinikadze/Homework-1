db1b9392a27f44c7f8d535fc1b8769272f54a034
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-13 23:10:57 +0400

    feature/complete media for plan.jsx

--- src/Components/Cards/PlanCard.jsx
+++ src/Components/Cards/PlanCard.jsx
@@ -3,20 +3,26 @@ import React from "react";
 const PlanCard = ({ cardId, cardTitle, price, duration, content }) => {
   return (
     <>
-      <li className="max-w-[413px] w-full p-[40px] bg-[#1A1A1A] rounded-[10px]">
-        <h1 className="text-[20px] font-bold mb-3 text-[#FFFFFF]">
+      <li className="md:max-w-[413px] lg:max-w-[512px] max-w-[358px] w-full md:p-[40px] lg:p-[50px] p-[24px] bg-[#1A1A1A] rounded-[10px]">
+        <h1 className="md:text-[20px] text-[18px] lg:text-[24px] font-bold md:mb-3 lg:mb-4 mb-[10px] text-[#FFFFFF]">
           {cardTitle}
         </h1>
-        <p className="text-[16px] text-[#999999] mb-[40px]">{content}</p>
+        <p className="md:text-[16px] lg:text-[18px] text-[14px] text-[#999999] mb-[40px]">
+          {content}
+        </p>
         <div className="mb-10">
-          <span className="text-[30px] font-semibold">{price}</span>
-          <span className="text-[#999999]">{duration}</span>
+          <span className="md:text-[30px] text-[24px] lg:text-[40px] font-semibold">
+            {price}
+          </span>
+          <span className="text-[#999999] lg:text-[18px] md:text-[16px] text-[14px]">
+            {duration}
+          </span>
         </div>
         <div className="flex max-w-[412px] w-full justify-between">
-          <button className="py-[18px] px-[35px] bg-[#141414] text-[#FFFFFF] rounded-[8px]">
+          <button className="md:py-[18px] md:px-[35px] px-[25px] py-[14px] text-[14px] bg-[#141414] text-[#FFFFFF] rounded-[8px]">
             Start Free Trial
           </button>
-          <button className="py-[18px] px-[35px] bg-[#E50000] text-[#FFFFFF] rounded-[8px]">
+          <button className="md:py-[18px] md:px-[35px] px-[25px] py-[14px] text-[14px] bg-[#E50000] text-[#FFFFFF] rounded-[8px]">
             Choose Plan
           </button>
         </div>
--- src/Components/Plan.jsx
+++ src/Components/Plan.jsx
@@ -5,25 +5,25 @@ import { plan } from "../constants";
 const Plan = () => {
   return (
     <>
-      <div className="text-white max-w-[1440px] w-full mx-auto flex justify-center mb-[120px]">
-        <div className="max-w-[1300px] w-full px-10px">
+      <div className="text-white md:max-w-[1440px] lg:max-w-[1920px] w-full mx-auto flex justify-center mb-[120px]">
+        <div className="max-w-[1280px] w-full px-[10px]">
           {/* Header */}
-          <div className="flex justify-between mb-[60px]">
-            <div>
-              <h1 className="text-[#FFFFFF] text-[28px] font-bold ">
+          <div className="flex flex-col md:flex-row justify-between mb-[40px] lg:mb-[80px] md:mb-[60px]">
+            <div className="mb-[20px] md:mb-0">
+              <h1 className="text-[#FFFFFF] mb:text-[28px] lg:text-[28px] text-[24px] font-bold ">
                 {plan.title}
               </h1>
-              <p className="text-[#999999]">{plan.subTitle}</p>
+              <p className="text-[#999999] mb:text-[18px] text-[14px]">
+                {plan.subTitle}
+              </p>
             </div>
-            <div className="max-w-[190px] w-full flex p-2 bg-[#0F0F0F] rounded-[8px]">
-              <div className="flex-[1] bg-[#1F1F1F] text-center rounded-[6px]">
-                Montly
-              </div>
+            <div className="max-w-[190px] lg:max-w-[237px] w-full h-[61px] flex items-center p-2 bg-[#0F0F0F] rounded-[8px]">
+              <div className="flex-[1] text-center rounded-[6px]">Montly</div>
               <div className="flex-[1] text-center">Yearly</div>
             </div>
           </div>
           <div>
-            <ul className="flex justify-between">
+            <ul className="flex flex-col md:flex-row gap-y-[20px] justify-between">
               {plan.content.map((item) => (
                 <PlanCard
                   key={item.id}
