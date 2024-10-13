82022fed3f88e91c989d584e2be739088bbd6d74
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-13 21:05:17 +0400

    style/change media screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ export default {
   theme: {
     extend: {},
     screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+      sm: "380px",
+      md: "1440px",
+      lg: "1902px",
     },
   },
   plugins: [],
