c696e2948aa9557236e5625f5b89ef19f2080070
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-13 21:58:35 +0400

    feature/complete media for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,13 +5,13 @@ import { headerTitle } from "../constants";
 const Header = () => {
   return (
     <>
-      <div className="h-[836px] bg-[url('./Assets/Background.png')] bg-contain bg-center bg-no-repeat mb-[130px]">
+      <div className="h-fit md:bg-[url('./Assets/Background.png')] bg-[url('./Assets/PhoneBg.png')]  bg-center bg-no-repeat md:mb-[130px]">
         <NavBar />
-        <div className="mx-auto flex flex-col justify-center items-center text-center text-[#FFFFFF] mt-[501px]">
-          <h1 className="text-[48px] leading-[72px] font-bold mb-[10px] ">
+        <div className="mx-auto flex flex-col justify-center items-center text-center text-[#FFFFFF] mt-[300px] md:mt-[501px]">
+          <h1 className="md:text-[48px] text-[28px] font-bold mb-[10px] ">
             {headerTitle.title}
           </h1>
-          <p className="text-[14px] px-2 mb-[40px] leading-[21px] max-w-[1140px] w-full text-[#999999]">
+          <p className="text-[14px] mb-[30px] md:mb-[40px] md:max-w-[1120px] w-full text-[#999999]">
             {headerTitle.subTitle}
           </p>
           <button className="py-[14px] px-[24px] bg-[#E50000] rounded-lg">
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,26 +7,34 @@ const NavBar = () => {
   return (
     <>
       <div className="flex justify-center mx-auto max-w-[1440px] w-full h-24 py-5">
-        <div className="max-w-[1260px]] w-full flex justify-between items-center px-[10px]">
+        <div className="max-w-[1577px] w-full flex justify-between items-center px-[10px]">
           <div className="">
-            <img src={Logo} alt="Logo" className="w-[116px] md:w-[165px]" />
+            <img
+              src={Logo}
+              alt="Logo"
+              className="w-[116px] md:w-[165px] lg:w-[200px]"
+            />
           </div>
-          <div className="items-center hidden sm:flex justify-center max-w-[412px] w-full h-[61px] border-[3px] border-[#1F1F1F] rounded-lg bg-[#0F0F0F]">
+          <div className="items-center hidden md:flex justify-center lg:max-w-[555px]  md:max-w-[412px] w-full lg:h-[75px] h-[61px] border-[3px] border-[#1F1F1F] rounded-lg bg-[#0F0F0F]">
             <nav className="">
-              <ul className="flex items-center h-11 gap-4 text-[#BFBFBF]">
+              <ul className="flex items-center md:h-11 lg:h-[55px] md:gap-4 lg:gap-[30px] text-[#BFBFBF]">
                 {navBar.map((item) => (
                   <li key={item.title}>
-                    <li className="">{item.title}</li>
+                    <li className="lg:text-[18px]">{item.title}</li>
                   </li>
                 ))}
               </ul>
             </nav>
           </div>
-          <div className="hidden sm:flex justify-between max-w-14 w-full h-6">
-            <img src={Search} alt="Search" />
-            <img src={Notification} alt="notification" />
+          <div className="hidden md:flex justify-between max-w-14 w-full h-6">
+            <img src={Search} alt="Search" className="md:w-[34px]" />
+            <img
+              src={Notification}
+              alt="notification"
+              className="lg:w-[34px]"
+            />
           </div>
-          <div className="sm:hidden">
+          <div className="md:hidden">
             <img src={burgerBar} alt="Menu" />
           </div>
         </div>
