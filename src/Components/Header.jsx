79398f4530793a60f72851a25661fb950a37e850
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-17 23:49:06 +0400

    feature/add different height

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { headerTitle } from "../constants";
 const Header = () => {
   return (
     <>
-      <div className="max-h-[836px] md:bg-[url('./Assets/Background.png')] bg-[url('./Assets/PhoneBg.png')] bg-center bg-no-repeat flex flex-col justify-between">
+      <div className="h-[100dvh] md:bg-[url('./Assets/Background.png')] bg-[url('./Assets/PhoneBg.png')] bg-center bg-no-repeat flex flex-col justify-between">
         <NavBar />
         <div className="mx-auto flex flex-col justify-center items-center text-center text-[#FFFFFF]">
           <h1 className="md:text-[48px] text-[28px] font-bold mb-[10px] ">
